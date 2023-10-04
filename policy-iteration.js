98ed72792f983765a925a1e89ce661f0051bbac4
Magnus <maxels19@student.aau.dk>
2023-10-04 08:51:09 +0200

    fixed while loop

--- policy-iteration.js
+++ policy-iteration.js
@@ -1,17 +1,37 @@
 //1. Initializations
 const S = [
-  [{ v: 0, id: 0 }, { v: -1, id: 1 }, { v: -2, id: 2 }, { v: -3, id: 3 }],
-  [{ v: -4, id: 4 }, { v: -5, id: 5 }, { v: -6, id: 6 }, { v: -7, id: 7 }],
-  [{ v: -8, id: 8 }, { v: -9, id: 9 }, { v: -10, id: 10 }, { v: -11, id: 11 }],
-  [{ v: -12, id: 12 }, { v: -13, id: 13 }, { v: -14, id: 14 }, { v: 0, id: 14 }],
+  [
+    { v: 0, id: 0 },
+    { v: -1, id: 1 },
+    { v: -2, id: 2 },
+    { v: -3, id: 3 },
+  ],
+  [
+    { v: -4, id: 4 },
+    { v: -5, id: 5 },
+    { v: -6, id: 6 },
+    { v: -7, id: 7 },
+  ],
+  [
+    { v: -8, id: 8 },
+    { v: -9, id: 9 },
+    { v: -10, id: 10 },
+    { v: -11, id: 11 },
+  ],
+  [
+    { v: -12, id: 12 },
+    { v: -13, id: 13 },
+    { v: -14, id: 14 },
+    { v: 0, id: 14 },
+  ],
 ];
 const actions = ["up", "down", "left", "right"];
 
-
-
 //2. Policy Evaluation
 let delta = 0;
-while (delta < 1) {
+let last_delta = null;
+while (last_delta == null || Math.abs(delta - last_delta) > 0.2) {
+  last_delta = delta;
   for (let i = 0; i < S.length; i++) {
     for (let j = 0; j < S[i].length; j++) {
       if ((i != 0 || j != 0) && (i != 3 || j != 3)) {
@@ -19,13 +39,11 @@ while (delta < 1) {
         S[i][j].v = calculate_V(S[i][j]);
         const value_def = Math.abs(v - S[i][j].v);
         delta = delta > value_def ? delta : value_def;
-        console.log(S[i][j].v)
+        console.log(S[0][2]);
       }
-
     }
   }
 }
-
 
 function calculate_V(s) {
   let v = 0;
@@ -37,7 +55,6 @@ function calculate_V(s) {
   });
   return v;
 }
-
 
 function get_next_state(s, a) {
   let state = null;
@@ -67,10 +84,10 @@ function get_next_state_v(s, a) {
 
 function get_state_by_id(id) {
   let state = null;
-  S.forEach(i => {
-    const s = i.find(state => state.id == id);
+  S.forEach((i) => {
+    const s = i.find((state) => state.id == id);
     if (s != null) state = s;
-  })
+  });
   return state;
 }
 
